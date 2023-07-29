2e7042317692b1850435bfd8c7908dd0acbc010f
seungchae <id6639@naver.com>
2023-07-29 21:13:29 +0900

    final

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import AuthContext from './context/AuthContext'
+import ActiveStatus from './components/ActiveStatus'
+import ToasterContext from './context/ToasterContext'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +19,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AuthContext>
+          <ToasterContext />
+          <ActiveStatus />
+          {children}
+        </AuthContext>
+      </body>
     </html>
   )
 }
---
+++ app/users/page.tsx
@@ -0,0 +1,11 @@
+import EmptyState from "../components/EmptyState";
+
+const Users = () => {
+  return (
+    <button className="hidden lg:block lg:pl-80 h-full">
+      <EmptyState />
+    </button>
+  );
+}
+
+export default Users;
